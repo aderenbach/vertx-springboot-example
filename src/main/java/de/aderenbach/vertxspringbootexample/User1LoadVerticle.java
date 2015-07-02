c830d7f267b870d0d712bf65308d8f520cdbffab
aderenbach@gmail.com <aderenbach@gmail.com>
2015-07-02 22:25:49 +0200

    added properties

--- src/main/java/de/aderenbach/vertxspringbootexample/User1LoadVerticle.java
+++ src/main/java/de/aderenbach/vertxspringbootexample/User1LoadVerticle.java
@@ -3,6 +3,7 @@ package de.aderenbach.vertxspringbootexample;
 import de.aderenbach.vertxspringbootexample.model.User;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Verticle;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -13,11 +14,14 @@ import java.util.UUID;
 @Component
 public class User1LoadVerticle extends AbstractVerticle implements Verticle {
 
+    @Value("${user1.name}")
+    private String username;
+
     @Override
     public void start() throws Exception {
 
         vertx.eventBus().consumer("user",m -> {
-            User r = new User(UUID.randomUUID().toString(),"User 1");
+            User r = new User(UUID.randomUUID().toString(),username);
             m.reply(r);
         });
 
--- src/main/java/de/aderenbach/vertxspringbootexample/User2LoadVerticle.java
+++ src/main/java/de/aderenbach/vertxspringbootexample/User2LoadVerticle.java
@@ -3,6 +3,7 @@ package de.aderenbach.vertxspringbootexample;
 import de.aderenbach.vertxspringbootexample.model.User;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Verticle;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -15,11 +16,14 @@ import java.util.UUID;
 @Profile("user2")
 public class User2LoadVerticle extends AbstractVerticle implements Verticle {
 
+    @Value("${user2.name}")
+    private String username;
+
     @Override
     public void start() throws Exception {
 
         vertx.eventBus().consumer("user",m -> {
-            User r = new User(UUID.randomUUID().toString(),"User 2");
+            User r = new User(UUID.randomUUID().toString(),username);
             m.reply(r);
         });
 
